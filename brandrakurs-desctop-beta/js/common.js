52441b56297cb315b5389b4c16d87cffbdec83df
NShishkov <hellsing0072007@Yandex.ru>
2019-09-19 13:41:33 +0300

    update animation

--- brandrakurs-desctop-beta/js/common.js
+++ brandrakurs-desctop-beta/js/common.js
@@ -11,38 +11,54 @@ $(function() {
 				var destination = $('.services').offset().top+1;
 				semaphore = false;
 				$([document.documentElement, document.body]).animate({
-		    	scrollTop: destination
-		    }, 1000, function(){
-		    	canScrollBot = false;
-			    canScrollTop = true;
-			    semaphore = true;
-			    $('.main-content').css('margin-left',320);
-			    $('.main-content').css('width',width);
-			    $('.menu-left').css('margin-left',0);
-		    });
-		    
+					scrollTop: destination
+				}, 1000, function(){
+					canScrollBot = false;
+					canScrollTop = true;
+					semaphore = true;
+					// $('.main-content').css('transform','translate3d(320px,0,0)');
+					// $('.main-content').css('width',width);
+					$('.menu-left').css('transform','translate3d(0px,0,0)');
+				});
+
 			}
 			if (canScrollTop  && (($(window).scrollTop() <= $('.section-top').height())) ) {
-				$('.main-content').css('width',$(window).width());
-	    	$('.main-content').css('margin-left',0);
-	    	$('.menu-left').css('margin-left',-320);
+				$('.menu-left').css('transform','translate3d(-320px,0,0)');
 				semaphore = false;
 				$([document.documentElement, document.body]).animate({
-		    	scrollTop: 0
-		    }, 1000,function(){
-		    	canScrollBot = true;
-		  	  canScrollTop = false;
-		    	semaphore = true;	
+					scrollTop: 0
+				}, 1000,function(){
+					canScrollBot = true;
+					canScrollTop = false;
+					semaphore = true;	
 
-		    });
+				});
 			}
-			// i++;
-			// console.log("iteration: " +i);
-			// console.log('csT: ' + canScrollTop);
-			// console.log('csB: ' + canScrollBot);
-			// console.log('semaphore: ' + semaphore);
+			i++;
+			console.log("iteration: " +i);
+			console.log('csT: ' + canScrollTop);
+			console.log('csB: ' + canScrollBot);
+			console.log('semaphore: ' + semaphore);
 		}
 
 	});
 
+
+	$('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});
 });
